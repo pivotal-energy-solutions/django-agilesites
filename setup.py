9fa99fe4046eb87367d358aa69cdc9202aafa0c9
Steven Klass <sklass@pointcircle.com>
2015-04-22 17:54:17 -0700

    Fixed a couple small things

--- setup.py
+++ setup.py
@@ -2,25 +2,33 @@
 """setup.py: Django django-agilesites"""
 
 from setuptools import find_packages, setup
+from django_agilesites import __name__, __version__, __author__
 
-setup(name='django-agilesites',
-      version='1.0',
-      description='This provides a django the ability of dynamic switching of templates and static directories based on host.',
-      author='Steven Klass',
-      author_email='sklass@pivotalenergysolutions.com',
-      license='Apache License (2.0)',
-      classifiers=[
-           'Development Status :: 2 - Pre-Alpha',
-           'Environment :: Web Environment',
-           'Framework :: Django',
-           'Intended Audience :: Developers',
-           'License :: OSI Approved :: Apache Software License',
-           'Operating System :: OS Independent',
-           'Programming Language :: Python',
-           'Topic :: Software Development',
-      ],
-      packages=find_packages(exclude=['tests', 'tests.*']),
-      package_data={'django_agilesites': ['static/js/*.js', 'templates/appsearch/*.html']},
-      include_package_data=True,
-      requires=['django (>=1.5)'],
-)
+base_url = 'https://github.com/pivotal-energy-solutions/django-agilesites'
+
+setup(
+    name=__name__,
+    version=__version__,
+    description='Simple-history: History for Django Models',
+    long_description=open('README.md').read(),
+    author=__author__,
+    author_email='sklass@pivotalenergysolutions.com',
+    url=base_url,
+    download_url='{0}/archive/{1}-{1}.tar.gz'.format(base_url, __name__, __version__),
+    license='Apache License (2.0)',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data={'django_agilesites': ['static/js/*.js']},
+    include_package_data=True,
+    requires=['django (>=1.5)'])
