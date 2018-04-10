000e8cee9566aed895764e411cf393d8c310067f
Ararat Khachatryan <ararat_khachatryan@instigatedesign.com>
2018-04-10 12:45:23 +0400

    Adopted to icm_ipcatalog Django2.0 ported version.

--- django_agilesites/loaders.py
+++ django_agilesites/loaders.py
@@ -23,34 +23,5 @@ class AgileSiteAppDirectoriesFinder(app_directories.Loader):
         """This will append the site-prefix to every application template directory.  If
         SITE_ROOT is defined in settings it will scope to your application.  This is to enable
         the file finder to work correctly"""
-
-        site_id = settings.SITE_ID
-        site_root = getattr(settings, 'SITE_ROOT')
-        site_prefixes = settings.SITE_FOLDERS.get(site_id, [])
-
-        if not self.logged:
-            log.debug("Getting sources = {0} ({1}) - {2} {3}".format(
-                site_id,  site_prefixes, template_name, template_dirs))
-            self.logged = True
-
-        if not isinstance(site_prefixes, (list, tuple)):
-            site_prefixes = [site_prefixes]
-
-        _app_template_dirs = []
-        if not template_dirs:
-            try:
-                # Post Django 1.8
-                template_dirs = app_directories.get_app_template_dirs('templates')
-            except AttributeError:
-                # Pre Django 1.8
-                template_dirs = app_directories.app_template_dirs
-
-        for app_dir in template_dirs:
-            if site_root and site_root not in app_dir:
-                continue
-            for site_prefix in site_prefixes:
-                _app_template_dirs.append(safe_join(app_dir, site_prefix))
-
-        _app_template_dirs = tuple(_app_template_dirs) if len(_app_template_dirs) else None
-        return super(AgileSiteAppDirectoriesFinder, self).get_template_sources(template_name, _app_template_dirs)
+        return super(AgileSiteAppDirectoriesFinder, self).get_template_sources(template_name)
 
